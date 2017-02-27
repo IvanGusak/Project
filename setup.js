16b818f7e4de2bf4a5a34d00c4e68e677cd879fc
IvanGusak <gusakiva@gmail.com>
2017-02-27 12:55:51 +0200

    adde to package socket.io

--- setup.js
+++ setup.js
@@ -1,5 +1,8 @@
 require("babel-register")();
-
+var socket = require('socket.io-client')('http://localhost');
+  socket.on('connect', function(){});
+  socket.on('event', function(data){});
+  socket.on('disconnect', function(){});
 var jsdom = require("jsdom").jsdom;
 
 app.get(/.*/, function root(req, res) {
