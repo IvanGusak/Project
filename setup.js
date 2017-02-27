af86124559db0c9dcaf742363bcfb3d5febc70c0
IvanGusak <gusakiva@gmail.com>
2017-02-27 13:53:32 +0200

    0.0.4 socket issue

--- setup.js
+++ setup.js
@@ -1,6 +1,6 @@
 require("babel-register")();
 
-var socket = require('socket.io-client')('https://ivangusak.github.io/Project/');
+var socket = require('socket.io-client')('http://localhost:3333');
   socket.on('connect', function(){});
   socket.on('event', function(data){});
   socket.on('disconnect', function(){});
